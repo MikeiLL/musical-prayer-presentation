1aad64806dbd291209080a5300cc8d451fc5ce24
mikeill <mike@mzoo.org>
2024-08-23 17:05:24 -0500

    Remove some files after render.

--- render.sh
+++ render.sh
@@ -1,7 +1,11 @@
 lilypond-book 8-25-24.lytex
 latex 8-25-24.tex
 dvipdf 8-25-24.dvi
-#rm 8-25-24.aux
-#rm 8-25-24.dvi
-#rm 8-25-24.log
+rm -r 13/
+rm 8-25-24.aux
+rm 8-25-24.dep
+rm 8-25-24.dvi
+rm 8-25-24.log
+rm 8-25-24.tex
+rm lock
 open 8-25-24.pdf
